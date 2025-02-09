e56f16506772c05e66bab8b69257e9fb5003f279
VitorLucioOliveira <vitor.lucio.0916@gmail.com>
2025-02-09 14:52:46 -0300

    Carrosel Chat gpt

--- static/MudarTema.js
+++ static/MudarTema.js
@@ -4,11 +4,13 @@ const botao_tema = document.querySelector('.botao_tema');
 const tema = document.querySelector('.titulo');
 const botao_dowload = document.querySelector('.botao_download');
 const seta = document.querySelector('.seta');
+const paragrafo = document.querySelector('.paragrafo_descricao');
 
 botao_tema.addEventListener('click', () => {
     document.body.classList.toggle('light');
     tema.classList.toggle('light');
     seta.classList.toggle('light_seta');
+    paragrafo.classList.toggle('paragrafo_descricao_light');
    
    
 });
